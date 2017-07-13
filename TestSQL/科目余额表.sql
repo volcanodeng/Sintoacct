45972ead126c497c7e05b9bcfa236b23e60a72a7
邓庆 <3035112@qq.com>
2017-07-13 11:50:24 +0800

    重新提交

---
+++ TestSQL/科目余额表.sql
@@ -0,0 +1,45 @@
+select min(a.AccCode),MIN(a.AccName),
+case when min(a.Direction)='質' then min(a.InitialBalance) end as initdebit,
+case when min(a.Direction)='湃' then min(a.InitialBalance) end as initcredit
+from T_Voucher v,T_Voucher_Detail vd,T_Account a
+where v.VId=vd.VId and vd.AccId=a.AccId and v.AbId='81084FA8-0D66-E711-826E-9C5C8E79F58D'
+group by a.AccId
+order by MIN(a.AccCode)
+
+
+
+select min(a.AccCode) as acccode,MIN(a.AccName) as accname,a.Direction,
+(select sum(case  a.Direction when '質' then SUM(vd.Debit-vd.Credit) when '湃' then SUM(vd.Credit-vd.Debit) end) from T_Voucher_Detail) as initamount,
+case  a.Direction when '質' then SUM(vd.Debit-vd.Credit) when '湃' then SUM(vd.Credit-vd.Debit) end as CurrentOccurrence
+
+from T_Voucher v,T_Voucher_Detail vd,T_Account a
+where v.VId=vd.VId and vd.AccId=a.AccId and v.AbId='81084FA8-0D66-E711-826E-9C5C8E79F58D'
+and v.[VoucherYear]=2017 and v.VoucherMonth >=6 and v.VoucherMonth<=7
+group by a.AccId,a.Direction
+order by MIN(a.AccCode)
+
+
+
+select vd.AccountCode,vd.AccountName,
+case  a.Direction when '質' then SUM(vd.Debit-vd.Credit) when '湃' then SUM(vd.Credit-vd.Debit) end 
+from T_Voucher v,T_Voucher_Detail vd,T_Account a 
+where v.VId=vd.VId and vd.AccId=a.AccId
+  and v.AbId='81084FA8-0D66-E711-826E-9C5C8E79F58D' 
+  and v.VoucherYear=2017 and v.VoucherMonth<6
+  group by vd.AccId,vd.AccountCode,vd.AccountName,a.Direction
+union
+select vd.AccountCode,vd.AccountName,
+case  a.Direction when '質' then SUM(vd.Debit-vd.Credit) when '湃' then SUM(vd.Credit-vd.Debit) end
+from T_Voucher v,T_Voucher_Detail vd,T_Account a
+where v.VId=vd.VId and vd.AccId=a.AccId 
+and v.AbId='81084FA8-0D66-E711-826E-9C5C8E79F58D'
+and v.[VoucherYear]=2017 and v.VoucherMonth >=6 and v.VoucherMonth<=7
+group by vd.AccId,vd.AccountCode,vd.AccountName,a.Direction
+union
+select vd.AccountCode,vd.AccountName,
+case  a.Direction when '質' then SUM(vd.Debit-vd.Credit) when '湃' then SUM(vd.Credit-vd.Debit) end 
+from T_Voucher v,T_Voucher_Detail vd,T_Account a 
+where v.VId=vd.VId and vd.AccId=a.AccId
+  and v.AbId='81084FA8-0D66-E711-826E-9C5C8E79F58D' 
+  and v.VoucherYear=2017 and v.VoucherMonth<=7
+  group by vd.AccId,vd.AccountCode,vd.AccountName,a.Direction
