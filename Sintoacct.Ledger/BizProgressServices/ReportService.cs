3e254dd638b348a8f5d212cddc11ef7be6f62fc4
邓庆 <3035112@qq.com>
2018-03-16 21:50:31 +0800

    凭证录入修缺

--- Sintoacct.Ledger/BizProgressServices/ReportService.cs
+++ Sintoacct.Ledger/BizProgressServices/ReportService.cs
@@ -20,7 +20,7 @@ namespace Sintoacct.Ledger.BizProgressServices
         public List<ProgressListViewModel> GetProgressList(ProgressSearchViewModel condition)
         {
             List<SqlParameter> parameters = new List<SqlParameter>(); 
-            string sql = "select top 500 o.[ContractTime],o.[BizManager],o.[BizOperations],o.[Recommend],o.[CommercialExpense],c.[CustomerName],i.ItemName,s.StepName,p.CompletedTime,p.ResultDesc,p.CreateTime,p.Creator,c.Contacts " +
+            string sql = "select top 5000 o.[ContractTime],o.[BizManager],o.[BizOperations],o.[Recommend],o.[CommercialExpense],c.[CustomerName],i.ItemName,s.StepName,p.CompletedTime,p.ResultDesc,p.CreateTime,p.Creator,c.Contacts " +
                          " from T_Prog_WorkOrder o left join T_Prog_Customers c on o.CusId=c.CusId left join T_Prog_WorkProgress p on o.WoId=p.WoId left join T_Prog_BizItems i on p.ItemId=i.ItemId left join T_Prog_BizSteps s on p.StepId=s.StepId" +
                          " where o.State>0 and p.ResultDesc is not null";
             if (condition.sCreate.HasValue) {
